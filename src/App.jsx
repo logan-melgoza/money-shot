ea0f5a7324476133f64ca57177f1a586279b430d
loganmelgoza <125330743+loganmelgoza@users.noreply.github.com>
2025-01-29 22:24:51 -0800

    Intial Commit of the Money Shot React App

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import Title from './Title';
+
+function App() {
+  return <div><Title></Title></div>;
+}
+export default App;
---
+++ src/Title.jsx
@@ -0,0 +1,5 @@
+function Title() {
+  return <h1>Money Shot - Sports Analysis</h1>;
+}
+
+export default Title;
